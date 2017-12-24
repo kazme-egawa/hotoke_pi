b7f6b3782ae48eec87c8bf6099018dbdec9ad294
kazme-egawa <arnerican.f0otboy@gmail.com>
2017-12-25 07:27:12 +0900

    hotoke

--- simple_client.py
+++ simple_client.py
@@ -1,6 +1,6 @@
 import liblo, sys
 try:
-    target = liblo.Address("192.168.1.255",5005)
+    target = liblo.Address("localhost",5005)
 except liblo.AddressError, err:
     print str(err)
     sys.exit()
